211e0a1e3640299c68e6fd68d4cec357133746a1
ChristopherAChase <Christopher.A.Chase@outlook.com>
2021-01-19 21:59:07 -0600

    removed unnecessary intermediate tables. Added projects table

--- schema.sql
+++ schema.sql
@@ -1,84 +1,72 @@
 CREATE TABLE Users(
-    UserID INTEGER PRIMARY KEY AUTOINCREMENT,
-    UserName TEXT UNIQUE NOT NULL,
-    Password TEXT NOT NULL,
-    Email TEXT UNIQUE NOT NULL,
-    FirstName TEXT NOT NULL,
-    LastName TEXT NOT NULL
+    UserID      INTEGER         PRIMARY KEY AUTOINCREMENT,
+    UserName    TEXT    UNIQUE  NOT NULL,
+    Password    TEXT            NOT NULL,
+    Email       TEXT    UNIQUE  NOT NULL,
+    FirstName   TEXT            NOT NULL,
+    LastName    TEXT            NOT NULL
 );
 
 CREATE TABLE Teams (
-    TeamID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name TEXT NOT NULL,
-    OwnerID INTEGER NOT NULL,
-    CreatedOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    Description TEXT NOT NULL, 
-    UNIQUE(Name, OwnerID)
+    TeamID      INTEGER     PRIMARY KEY AUTOINCREMENT,
+    Name        TEXT        NOT NULL,
+    OwnerID     INTEGER     NOT NULL,
+    CreatedOn   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Description TEXT        NOT NULL DEFAULT 'New Team', 
+    UNIQUE(Name, OwnerID),
+    FOREIGN KEY (OwnerID) REFERENCES Users (UserID)
 );
 
 CREATE TABLE Projects (
-    ProjectID INTEGER PRIMARY KEY AUTOINCREMENT,
-    ProjectName TEXT NOT NULL,
-    CreatedOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    Description TEXT NOT NULL
-
-)
+    ProjectID   INTEGER     PRIMARY KEY AUTOINCREMENT,
+    Project     TEXT        NOT NULL,
+    Description TEXT        NOT NULL DEFAULT 'New project',
+    CreatedOn   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    TeamID      INTEGER     NOT NULL,
+    FOREIGN KEY (TeamID) REFERENCES Teams (TeamID)
+);
 
 CREATE TABLE Tasks (
-    TaskID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Task TEXT NOT NULL,
-    CreatedOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CreatedBy INTEGER NOT NULL,
-    IsCompleted INTEGER NOT NULL DEFAULT 0,
+    TaskID      INTEGER     PRIMARY KEY AUTOINCREMENT,
+    Task        TEXT        NOT NULL,
+    CreatedOn   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CreatedBy   INTEGER     NOT NULL,
+    IsCompleted INTEGER     NOT NULL DEFAULT 0,
+    ProjectID   INTEGER     NOT NULL,
     FOREIGN KEY (CreatedBy) REFERENCES Users (UserID),
+    FOREIGN KEY (ProjectID) REFERENCES Projects (ProjectID),
     CHECK (isCompleted == 0 or isCompleted == 1)
 );
 
+CREATE TABLE Comments (
+    CommentID   INTEGER     PRIMARY KEY AUTOINCREMENT,
+    CommentText TEXT        NOT NULL,
+    CommentedBy INTEGER     NOT NULL,
+    CreatedOn   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    TaskID      INTEGER     NOT NULL,
+    FOREIGN KEY (CommentedBy) REFERENCES Users (UserID)
+    FOREIGN KEY (TaskID) REFERENCES Tasks (TaskID)
+);
+
 CREATE TABLE UserTeams (
-    UserTeamID INTEGER PRIMARY KEY AUTOINCREMENT,
-    UserID INTEGER NOT NULL,
-    TeamID INTEGER NOT NULL,
+    UserTeamID  INTEGER     PRIMARY KEY AUTOINCREMENT,
+    UserID      INTEGER     NOT NULL,
+    TeamID      INTEGER     NOT NULL,
     FOREIGN KEY (UserID) REFERENCES Users (UserID),
     FOREIGN KEY (TeamID) REFERENCES Teams (TeamID)
 );
 
 
-CREATE TABLE TeamsTasks (
-    TeamTaskID INTEGER PRIMARY KEY AUTOINCREMENT,
-    TeamID INTEGER NOT NULL,
-    TaskID INTEGER NOT NULL,
-    FOREIGN KEY (TeamID) REFERENCES Teams (TeamID),
-    FOREIGN KEY (TaskID) REFERENCES Tasks (TaskID)
-);
-
-
 CREATE TABLE TaskHistory (
-    TaskID INTEGER NOT NULL,
-    ModifiedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    ChangeType TEXT NOT NULL,
-    ChangedBy INTEGER NOT NULL,
-    OldText TEXT NOT NULL,
-    NewText TEXT NOT NULL,
+    TaskID          INTEGER     NOT NULL,
+    ModifiedDate    TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ChangeType      TEXT        NOT NULL,
+    ChangedBy       INTEGER     NOT NULL,
+    OldText         TEXT        NOT NULL,
+    NewText         TEXT        NOT NULL,
     FOREIGN KEY (TaskID) REFERENCES Tasks (TaskID),
     FOREIGN KEY (ChangedBy) REFERENCES Users (UserID),
     PRIMARY KEY (TaskID, ModifiedDate),
     CHECK(ChangeType IN ('A', 'C', 'D', 'M'))
 );
 
-
-CREATE TABLE Comments (
-    CommentID INTEGER PRIMARY KEY AUTOINCREMENT,
-    CommentText TEXT NOT NULL,
-    CommentedBy INTEGER NOT NULL,
-    FOREIGN KEY (CommentedBy) REFERENCES Users (UserID)
-);
-
-
-CREATE TABLE TaskComments (
-    TaskCommentID INTEGER PRIMARY KEY AUTOINCREMENT,
-    CommentID INTEGER NOT NULL,
-    TaskID INTEGER NOT NULL,
-    CreatedOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (CommentID) REFERENCES Comments (CommentID),
-    FOREIGN KEY (TaskID) REFERENCES Tasks (TaskID)
-);
